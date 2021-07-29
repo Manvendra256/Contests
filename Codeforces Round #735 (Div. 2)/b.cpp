51fd876b28d85c8a69f9114a2aeff43ae7061bd0
Manvendra256 <manvendra256@gmail.com>
2021-07-29 23:40:03 +0530

    CF Round 735 (Div-2)

---
+++ Codeforces Round #735 (Div. 2)/b.cpp
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <cmath>
+#include <set>
+#include <stack>
+#include <queue>
+#include <numeric>
+#include <deque>
+#include <cstring>
+#include <iterator>
+#include <map>
+#include <cstdlib>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <complex>
+#include <bitset>
+#include <chrono>
+#include <random>
+#include <assert.h>
+#include <array>
+#include <functional>
+
+using namespace std;
+
+template<typename M, typename N>
+istream& operator>>(istream&is, pair<M,N> & p){is >> p.first >> p.second; return is;}
+template<typename M>
+istream& operator>>(istream&is, vector<M> & v){for(auto &it:v) is >> it; return is; }
+template<typename M>
+istream& operator>>(istream&is, vector<vector<M>> & v){for(auto &it:v) is >> it; return is; }
+template<typename M, typename N>
+ostream& operator<<(ostream&os, const pair<M,N> &p){os << p.first << ' ' << p.second << '\n';return os;}
+template<typename M>
+ostream& operator<<(ostream&os, const vector<M> &v){for(auto it: v)os << it << ' ';cout << '\n';return os;}
+template<typename M>
+ostream& operator<<(ostream&os, const vector<vector<M>> &v){for(auto it: v)os << it;return os;}
+
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) (x).begin(),(x).end()
+#define mn(x) *min_element((x).begin(),(x).end())
+#define mx(x) *max_element((x).begin(),(x).end())
+#define acc(x) accumulate((x).begin(),(x).end(),0ll)
+#define unique(x) sort(all(x)); x.erase(unique(all(x)),x.end());
+#define eb emplace_back
+#define el '\n'
+typedef  long long  ll;
+typedef  unsigned long long  ull;
+typedef long double ld;
+typedef complex<long double> cd;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+typedef vector<pll> vpll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<bool> vb;
+typedef vector<string> vs;
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int a, int b){ return rng()%(b-a+1)+a;}
+
+const ld eps=1e-7;
+const ld pi=acos(-1);
+const ll inf=(ll)1e18+10;
+//const int mod=1000000007;
+const int mod=998244353;
+
+int n, m, k;
+const int N=2e3+10;
+
+
+
+int main()
+{   ios_base::sync_with_stdio(false);cin.tie(NULL);
+    int t;
+    cin >> t;
+    while(t--){
+        int n, m, k;
+        cin >> n >> k;
+        m = 32-__builtin_clz(n);
+        m = (1<<m);
+        vpi v(m, {-1,-1});
+        for(int i=1, u; i<=n; i++){
+            cin >> u;
+            if(v[u].fi==-1){
+                v[u].fi = i;
+            }
+            else{
+                v[u].se = v[u].fi;
+                v[u].fi = i;
+            }
+        }
+        ll ans=-inf;
+        for(int i=0; i<m; i++){
+            pll p({-1,-1});
+            for(int j=i; j; j=(j-1)&i){
+                if(v[j].fi>p.se){
+                    p.se = v[j].fi;
+                    if(p.fi<p.se)swap(p.fi, p.se);
+                }
+                if(v[j].se>p.se){
+                    p.se = v[j].se;
+                    if(p.fi<p.se)swap(p.fi, p.se);
+                }
+            }
+            if(v[0].fi>p.se){
+                p.se = v[0].fi;
+                if(p.fi<p.se)swap(p.fi, p.se);
+            }
+            if(v[0].se>p.se){
+                p.se = v[0].se;
+                if(p.fi<p.se)swap(p.fi, p.se);
+            }
+            if(p.fi==-1 || p.se==-1)continue;
+            ans = max(ans, p.fi*p.se-k*i);
+        }
+        cout << ans << el;
+    }
+    return 0;
+}
